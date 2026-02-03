665586cd14d980849881a98727942ee6fd14e721
jaaajaaaja <andkob0126@gmail.com>
2026-02-03 18:23:23 +0100

    added login

---
+++ src/api/auth.ts
@@ -0,0 +1,29 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
+export async function login(email: string, pass: string) {
+    try {
+        const res = await fetch(`${API_BASE_URL}/auth/login`, {
+            method: "POST",
+            body: JSON.stringify({ email, password: pass }),
+            headers: { "Content-Type": "application/json" },
+            credentials: "include"
+        })
+
+        if(res.status != 200) {
+            return res.json()
+        }
+
+    } catch (err) {
+        return err
+    }
+}
+
+export async function logout() {
+    const res = await fetch(`${API_BASE_URL}/auth/logout`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include"
+    })
+
+    return res.json()
+}
